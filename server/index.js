5db61ed7afbee306b475e59554cf60fae647a483
Cairo Campos <cairocampos98@gmail.com>
2022-03-14 10:02:00 -0300

    configure logger

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import server from './server.js'
+import { loggger } from './util.js'
 
 const PORT = 3000;
 
 server
 .listen(PORT)
-.on('listening', () => console.log(`Server is running at port: ${PORT}`))
+.on('listening', () => loggger.info(`Server is running at port: ${PORT}`))
